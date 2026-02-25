eb5d25b3f0472266d1701435af35377a40543949
Ritwik Biswas <biswas.ritwik2001@gmail.com>
2026-02-25 13:01:46 +0530

    Upadte the sendtoken function for http

--- backend/utils/sendToken.js
+++ backend/utils/sendToken.js
@@ -9,8 +9,8 @@ export const sendToken = (res,user,message,statusCode = 200) => {
     const options = {
         expires : new Date(Date.now() + 15*24*60*60*1000),
         httpOnly : true,
-        secure:true,
-        sameSite:"none",
+        secure: false,        // ✅ change this
+        sameSite: "lax",      // ✅ change this
 
     };
 

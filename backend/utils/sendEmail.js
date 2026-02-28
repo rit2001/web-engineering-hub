50148e9c5d88e8ce7368638f9d9bffebb8e6c050
Ritwik Biswas <biswas.ritwik2001@gmail.com>
2026-02-28 19:10:56 +0530

    Fixed the sendmail function uisng number

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,22 +1,21 @@
-import {createTransport } from "nodemailer";
+import { createTransport } from "nodemailer";
 
-export const sendEmail =async (to,subject,text) => {
+export const sendEmail = async (to, subject, text) => {
 
-    const transporter = createTransport(
-        {
-        host:process.env.SMTP_HOST,
-        port:process.env.SMTP_PORT ,
+    const transporter = createTransport({
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        secure: Number(process.env.SMTP_PORT) === 465, // ✅ important
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
         },
-        }
-    );
+    });
 
     await transporter.sendMail({
+        from: process.env.SMTP_USER, // better than hardcoding gmail
         to,
         subject,
         text,
-        from : "myid@gmail.com"
     });
 };
